1a74c3f193716401f9b1f105e9b04c6886a483a2
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-02-24 00:24:10 +0100

    fix: remove leading indentation from module.py

--- module.py
+++ module.py
@@ -1,31 +1,31 @@
-    from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
-    MODULE_ID = 'fleet'
-    MODULE_NAME = _('Fleet Management')
-    MODULE_VERSION = '1.0.0'
-    MODULE_ICON = 'car-sport-outline'
-    MODULE_DESCRIPTION = _('Vehicle fleet tracking, fuel and maintenance')
-    MODULE_AUTHOR = 'ERPlora'
-    MODULE_CATEGORY = 'operations'
+MODULE_ID = 'fleet'
+MODULE_NAME = _('Fleet Management')
+MODULE_VERSION = '1.0.0'
+MODULE_ICON = 'car-sport-outline'
+MODULE_DESCRIPTION = _('Vehicle fleet tracking, fuel and maintenance')
+MODULE_AUTHOR = 'ERPlora'
+MODULE_CATEGORY = 'operations'
 
-    MENU = {
-        'label': _('Fleet Management'),
-        'icon': 'car-sport-outline',
-        'order': 58,
-    }
+MENU = {
+    'label': _('Fleet Management'),
+    'icon': 'car-sport-outline',
+    'order': 58,
+}
 
-    NAVIGATION = [
-        {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
+NAVIGATION = [
+    {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
 {'label': _('Vehicles'), 'icon': 'car-sport-outline', 'id': 'vehicles'},
 {'label': _('Settings'), 'icon': 'settings-outline', 'id': 'settings'},
-    ]
+]
 
-    DEPENDENCIES = []
+DEPENDENCIES = []
 
-    PERMISSIONS = [
-        'fleet.view_vehicle',
+PERMISSIONS = [
+    'fleet.view_vehicle',
 'fleet.add_vehicle',
 'fleet.change_vehicle',
 'fleet.delete_vehicle',
 'fleet.manage_settings',
-    ]
+]
